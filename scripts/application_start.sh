bbabad4db29b1d9921503d7591a712c545832b6a
ash <ashrithjacob@gmail.com>
2024-12-23 02:49:04 +0530

    adjust scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo 'run application_start.sh: ' >>  /home/ubuntu/packet_analysis/deploy.log
+cd /home/ubuntu/packet_analysis >> /home/ubuntu/packet_analysis/deploy.log
 # nodejs-app is the same name as stored in pm2 process
 echo 'start docker containers' >> /home/ubuntu/packet_analysis/deploy.log
 docker compose up  >> /home/ubuntu/packet_analysis/deploy.log
