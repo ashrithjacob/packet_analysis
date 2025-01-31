e65d2a6ac04ce5c802436561e508b1dd72a51f77
ash <ashrithjacob@gmail.com>
2025-01-31 15:56:14 +0530

    demo:2.3

--- pull_ec2.sh
+++
@@ -1,2 +0,0 @@
-aws ecr get-login-password --region us-west-2 --profile nanites | docker login --username AWS --password-stdin 122610501872.dkr.ecr.us-west-2.amazonaws.com/pcap2
-docker pull 122610501872.dkr.ecr.us-west-2.amazonaws.com/pcap2:latest
